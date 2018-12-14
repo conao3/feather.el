14b379f3c3ef26426d669662da6a1ef6bbf9728e
conao3 <conao3@gmail.com>
2018-12-14 23:21:34 +0900

    define feather working directory

--- feather.el
+++ feather.el
@@ -35,19 +35,45 @@
 ;;
 
 (unless (fboundp 'gnutls-available-p)
-  (defun gnutls-available-p () nil))
+  (defun gnutls-available-p ()
+    "Available status for gnutls.
+(It is quite difficult to implement, so always return nil when not defined
+see `gnutls-available-p'.)"
+    nil))
+
+(unless (boundp 'user-emacs-directory)
+  (defvar user-emacs-directory
+    (if load-file-name
+        (expand-file-name (file-name-directory load-file-name))
+      "~/.emacs.d/")))
+
+(unless (fboundp 'locate-user-emacs-file)
+  (defun locate-user-emacs-file (name)
+    "Simple implementation of `locate-user-emacs-file'."
+    (format "%s%s" user-emacs-directory name)))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;
 ;;  Customizable variables
 ;;
 
-(defcustom leaf-archives `(("gnu" . ,(format "http%s://elpa.gnu.org/packages/"
-                                          (if (gnutls-available-p) "s" ""))))
+(defcustom feather-archives
+  `(("gnu" . ,(format "http%s://elpa.gnu.org/packages/"
+                      (if (gnutls-available-p) "s" ""))))
   "An alist of archives from which to fetch.
 If there are multiple download destinations, value top of the list is adopted"
   :type '(alist :key-type (string :tag "Archive name")
                 :value-type (string :tag "URL or directory name"))
+  :group 'feather)
+
+(defcustom feather-working-dir (locate-user-emacs-file "feather-repo")
+  "Directory is located download Emacs Lisp packages path."
+  :type 'directory
+  :group 'feather)
+
+(defcustom feather-build-dir (locate-user-emacs-file "feather-build")
+  "Directory is located byte-compiled Emacs Lisp files path."
+  :type 'directory
   :group 'feather)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
