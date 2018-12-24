e3999877e16ec7215ffdcdb97461b763a0c09e3d
conao3 <conao3@gmail.com>
2018-12-25 02:46:55 +0900

    add reference url

--- feather.el
+++ feather.el
@@ -333,7 +333,9 @@ If CMDLST is (A B C), if A fails, B and subsequent commands will not execute."
        ("git" "pull" "origin" "master")))))
 
 (defun feather-git-unshalow (pkg destpath)
-  "Unshallow repository to fetch whole repository."
+  "Unshallow repository to fetch whole repository.
+
+see https://stackoverflow.com/questions/37531605/how-to-test-if-git-repository-is-shallow"
   (let ((default-directory (expand-file-name destpath)))
     (feather-command-queue
      pkg
