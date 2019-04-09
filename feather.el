b20eac1f7f9b0666a36b879e6247bbc7ad004ca2
conao3 <conao3@gmail.com>
2019-04-10 01:25:45 +0900

    remove fit-clone

--- feather.el
+++ feather.el
@@ -279,15 +279,6 @@ If CMDLST is (A B C), if A fails, B and subsequent commands will not execute."
 
 ;; (feather-git-clone-head "melpa" "https://github.com/melpa/melpa" feather-recipes-dir)
 ;; (feather-git-clone "feather.el" "https://github.com/conao3/feather.el.git" feather-recipes-dir)
-(defun feather-git-clone (pkg url dir &optional shallow-p)
-  "Clone PKG repository from URL on DIR."
-  (unless (file-directory-p (expand-file-name pkg dir))
-    (feather-command-queue
-     `(("cd" ,dir)
-       ("pwd")
-       ("echo" "[Cloning]" ,name "...")
-       ("git" "clone" ,url ,@(when shallow-p '("--depth" "1")))))))
-
 (defun feather-git-full-clone (pkg url dir)
   "Clone PKG repository from URL on DIR. (full-clone)"
   (unless (file-directory-p (expand-file-name pkg dir))
