d092720f3460a89054f83f3ad5758f48e71d0966
conao3 <conao3@gmail.com>
2019-04-10 17:01:38 +0900

    add fetch-recipe test

--- feather-tests.el
+++ feather-tests.el
@@ -107,6 +107,19 @@
                   (feather-selected-packages-list ',feather-selected-packages-list)
                   (feather-pinned-packages-alist  ',feather-pinned-packages-alist)))))))
 
+(cort-deftest feaher:fetch-recipe
+  `((:string= "#s(hash-table size 65 test eq rehash-size 1.5 rehash-threshold 0.8125 data 
+   (zzz-to-char (:fetcher \"github\" :repo \"mrkkrp/zzz-to-char\" :files nil)
+    zygospore (:fetcher \"github\" :repo \"LouisKottmann/zygospore.el\" :files nil)
+    zweilight-theme (:fetcher \"github\" :repo \"philiparvidsson/Zweilight-Theme-for-Emacs\" :files nil)
+    ztree (:fetcher \"github\" :repo \"fourier/ztree\" :files nil)
+    zprint-mode (:fetcher \"github\" :repo \"pesterhazy/zprint-mode.el\" :files nil)
+   ))
+"
+              (feather-fetch-recipe
+               "lite"
+               "https://raw.githubusercontent.com/conao3/feather-recipes/master/recipes/lite.el"))))
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;
 ;;  simple test
