8e417e0525244b1756e7b23a78085d61d9598ef3
conao3 <conao3@gmail.com>
2019-06-04 05:14:41 +0900

    fix header

--- feather.el
+++ feather.el
@@ -1,26 +1,30 @@
-;;; feather.el ---                                   -*- lexical-binding: t; -*-
+;;; feather.el --- Parallel thread modern Emacs package manager        -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2018  Naoya Yamashita
+;; Copyright (C) 2018-2019  Naoya Yamashita
 
-;; Author: Naoya Yamashita
-;; Keywords: .emacs
+;; Author: Naoya Yamashita <conao3@gmail.com>
+;; Maintainer: Naoya Yamashita <conao3@gmail.com>
+;; Keywords: elisp package
+;; Version: 0.1.0
+;; URL: https://github.com/conao3/feather.el
+;; Package-Requires: ((emacs "24.4"))
 
-;; This program is free software; you can redistribute it and/or modify
-;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation, either version 3 of the License, or
-;; (at your option) any later version.
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the Affero GNU General Public License as
+;; published by the Free Software Foundation, either version 3 of the
+;; License, or (at your option) any later version.
 
-;; This program is distributed in the hope that it will be useful,
-;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;; GNU General Public License for more details.
+;; This program is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+;; See the Affero GNU General Public License for more details.
 
-;; You should have received a copy of the GNU General Public License
-;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+;; You should have received a copy of the Affero GNU General Public License
+;; along with this program.  If not, see  <https://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 
-;;
+;; Parallel thread modern Emacs package manager.
 
 ;;; Code:
 
