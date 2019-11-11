1ef392dff285b8b9c991eee9352fc0c8ff123a7a
conao3 <conao3@gmail.com>
2019-11-11 14:28:59 +0900

    add package-install around advice

--- feather.el
+++ feather.el
@@ -29,12 +29,51 @@
 
 ;;; Code:
 
+(require 'package)
 (require 'async-await)
 (require 'feather-polyfill)
 
 (defgroup feather nil
   "Parallel thread modern Emacs package manager."
   :group 'lisp)
+
+(defun feather--advice-package-install (_fn &rest args)
+  "Around advice for FN with ARGS.
+This code based package.el bundled with Emacs 26.3.
+See `package-install'."
+  (interactive
+   (progn
+     ;; Initialize the package system to get the list of package
+     ;; symbols for completion.
+     (unless package--initialized
+       (package-initialize t))
+     (unless package-archive-contents
+       (package-refresh-contents))
+     (list (intern (completing-read
+                    "Install package: "
+                    (delq nil
+                          (mapcar (lambda (elt)
+                                    (unless (package-installed-p (car elt))
+                                      (symbol-name (car elt))))
+                                  package-archive-contents))
+                    nil t))
+           nil)))
+  (pcase-let ((`(,pkg ,dont-select) args))
+    (add-hook 'post-command-hook #'package-menu--post-refresh)
+    (let ((name (if (package-desc-p pkg)
+                    (package-desc-name pkg)
+                  pkg)))
+      (unless (or dont-select (package--user-selected-p name))
+        (package--save-selected-packages
+         (cons name package-selected-packages)))
+      (if-let* ((transaction
+                 (if (package-desc-p pkg)
+                     (unless (package-installed-p pkg)
+                       (package-compute-transaction (list pkg)
+                                                    (package-desc-reqs pkg)))
+                   (package-compute-transaction () (list (list pkg))))))
+          (package-download-transaction transaction)
+        (message "`%s' is already installed" name)))))
 
 (provide 'feather)
 ;;; feather.el ends here
