5eea1b530da0f2ef18d860229e457ce5fd59e2d0
conao3 <conao3@gmail.com>
2020-02-20 02:27:56 +0900

    split feather--install-package from *-install-packages

--- feather.el
+++ feather.el
@@ -159,6 +159,30 @@ see `package-unpack'."
           (list :package (package-desc-name pkg-desc))))
        (funcall resolve pkg-dir)))))
 
+(async-defun feather--install-package (pkg)
+  "Install PKG async.
+PKG is `package-desc'."
+  (let ((name (package-desc-name pkg)))
+    (condition-case err
+        (let* ((res (await (feather--promise-change-queue-state name 'install)))
+               (res (await (feather--promise-install-package pkg)))
+               (res (await (feather--promise-activate-package pkg)))
+               (res (await (feather--promise-change-queue-state name 'done)))))
+      (error
+       (pcase err
+         (`(error (fail-install-package ,reason))
+          (ppp-debug :level :warning 'feather
+            "Cannot install package\n%s"
+            (ppp-plist-to-string
+             (list :package name
+                   :reason reason))))
+         (_
+          (ppp-debug :level :warning 'feather
+            "Something wrong while installing package\n%s"
+            (ppp-plist-to-string
+             (list :package name
+                   :reason err)))))))))
+
 (async-defun feather--install-packages (pkgs)
   "Install PKGS async.
 PKGS is `package-desc' list as (a b c).
@@ -197,26 +221,7 @@ see `package-install' and `package-download-transaction'."
 
     ;; `package-download-transaction'
     (dolist (pkg pkgs)
-      (let ((name (package-desc-name pkg)))
-        (condition-case err
-            (let* ((res (await (feather--promise-change-queue-state name 'install)))
-                   (res (await (feather--promise-install-package pkg)))
-                   (res (await (feather--promise-activate-package pkg)))
-                   (res (await (feather--promise-change-queue-state name 'done)))))
-          (error
-           (pcase err
-             (`(error (fail-install-package ,reason))
-              (ppp-debug :level :warning 'feather
-                "Cannot install package\n%s"
-                (ppp-plist-to-string
-                 (list :package name
-                       :reason reason))))
-             (_
-              (ppp-debug :level :warning 'feather
-                "Something wrong while installing package\n%s"
-                (ppp-plist-to-string
-                 (list :package name
-                       :reason err)))))))))
+      (await (feather--install-package pkg)))
 
     ;; decrement current-pallarel-process-number
     (cl-decf feather-current-pallarel-process-number)
