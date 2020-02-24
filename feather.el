9b3bb0b174db5f153d514e0fc0f7ef4736af3c5c
conao3 <conao3@gmail.com>
2020-02-24 12:42:11 +0900

    move change UI to out of while loop

--- feather.el
+++ feather.el
@@ -412,14 +412,14 @@ see `package-install' and `package-download-transaction'."
     (dolist (pkgdesc pkg-descs)
       (let ((pkg-name (package-desc-name pkgdesc)))
         (when-let (alist (gethash pkg-name feather-install-queue))
+          (feather--dashboard-change-item-state .target-pkg 'wait
+                                                `((dep-pkg . ,pkg-name)))
           (while (not (eq 'done (alist-get 'status alist)))
             (ppp-debug 'feather
               "Wait for dependencies to be installed\n%s"
               (ppp-plist-to-string
                (list :package pkg-name
                      :dependency-from .target-pkg)))
-            (feather--dashboard-change-item-state .target-pkg 'wait
-                                                  `((dep-pkg . ,pkg-name)))
             (await (promise:delay 0.5))))))
 
     ;; set the status of the package to be installed to queue
