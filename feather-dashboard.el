e837259ec1b933dbe46e9d85ba9b1efe96d2fdbc
conao3 <conao3@gmail.com>
2020-02-25 01:43:09 +0900

    declare-function from feather

--- feather-dashboard.el
+++ feather-dashboard.el
@@ -125,6 +125,16 @@ Key is package symbol, value is overlay.")
 
 ;;; functions
 
+(declare-function feather--change-running-state "feather")
+(declare-function feather--get-feather-running "feather")
+(declare-function feather--push-package-install-args "feather")
+(declare-function feather--pop-package-install-args "feather")
+(declare-function feather--get-package-install-args "feather")
+(declare-function feather--add-install-queue "feather")
+(declare-function feather--get-install-queue "feather")
+(declare-function feather--change-install-queue-status "feather")
+(declare-function feather--get-install-queue-status "feather")
+
 (defmacro with-feather-dashboard-buffer (&rest body)
   "Execute the forms in BODY with BUFFER-OR-NAME temporarily current.
 BUFFER-OR-NAME must be a buffer or the name of an existing buffer.
